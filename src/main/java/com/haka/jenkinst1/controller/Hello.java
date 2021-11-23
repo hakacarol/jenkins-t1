49f6a2b32e754702a2faa1a278f102f65910b015
hakacarol <haka.carolina@gmail.com>
2021-11-23 12:21:22 -0300

    criado classe Hello

---
+++ src/main/java/com/haka/jenkinst1/controller/Hello.java
@@ -0,0 +1,13 @@
+package com.haka.jenkinst1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/hello")
+    public String message() {
+        return "Hello World";
+    }
+}
