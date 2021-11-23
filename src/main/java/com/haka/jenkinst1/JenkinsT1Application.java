fa5dd83b5c537c178ac53a96e5a57e5f2c03031c
hakacarol <haka.carolina@gmail.com>
2021-11-23 11:11:27 -0300

    projeto criado

---
+++ src/main/java/com/haka/jenkinst1/JenkinsT1Application.java
@@ -0,0 +1,13 @@
+package com.haka.jenkinst1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsT1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsT1Application.class, args);
+	}
+
+}
